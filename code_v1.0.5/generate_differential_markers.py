959a6cb47022158ae051a67c4d8d1d52cea67ca7
QuKunLab <libin03@ustc.edu.cn>
2019-05-11 11:48:47 +0800

    update 2019-5-11

--- code_v1.0.5/generate_differential_markers.py
+++ code_v1.0.5/generate_differential_markers.py
@@ -17,17 +17,19 @@ usage = "Enriched motifs/genes/peaks of a cluster (batch)\nusage: %prog -s proje
 opts = OptionParser(usage=usage, version="%prog 1.0")
 opts.add_option("-s", help="The project folder.")
 opts.add_option("--cfile", help="cluster.csv file of a clustering method, e.g. louvain_cluster_by_Accesson.csv in result folder")
-opts.add_option("--cluster", help="The cluster for specific markers analysis, can be {0, 1, ..., nCluster}, or a batch of clusters like 0,2,3")
-opts.add_option("--vs", default='all', help="vs which clusters to search specific markers for target clusters, e.g. 1,4,2, default=all")
-opts.add_option("--pvalue", default=0.001, help='P-value threshold for specific markers, default=0.001')
-opts.add_option("--fold", default=2, help='Fold change cutoff of specific markers, default=2')
+opts.add_option("--cluster", help="The cluster for differential markers analysis, can be {0, 1, ..., nCluster}, or a batch of clusters like 0,2,3")
+opts.add_option("--vs", default='all', help="vs which clusters to search differential markers for target clusters, e.g. 1,4,2, default=all")
+opts.add_option("--pvalue", default=0.001, help='P-value threshold for differential markers, default=0.001')
+opts.add_option("--fold", default=2, help='Fold change cutoff of differential markers, default=2')
 opts.add_option("--motif", default='no', help='Whether to search differential motifs for target cluster, default=no.')
 opts.add_option("--gene", default='no', help='Whether to search differential genes for target cluster, default=no.')
 options, arguments = opts.parse_args()
 #
 #
 def group_cells(options):
-    deviation = pandas.read_csv(options.s+'/result/deviation_chromVAR.csv', sep=',', index_col=0,
+#    deviation = pandas.read_csv(options.s+'/result/deviation_chromVAR.csv', sep=',', index_col=0,
+#                   engine='c', na_filter=False, low_memory=False)
+    cells_df = pandas.read_csv(options.s+'/matrix/filtered_cells.csv', sep='\t', index_col=0,
                    engine='c', na_filter=False, low_memory=False)
     cluster_df = pandas.read_csv(options.cfile, sep='\t', index_col=0)
     kCluster = options.cluster.split(',')
@@ -38,7 +40,7 @@ def group_cells(options):
         kCluster = map(int, kCluster)
         if options.vs!='all': vsCluster = map(int, vsCluster)
     if options.vs=='all': vsCluster = list(set(cluster_df['cluster'].values)-set(kCluster))
-    cluster_df = cluster_df.loc[deviation.columns.values]
+    cluster_df = cluster_df.loc[cells_df.index.values]
     cell_inCluster = cluster_df.loc[cluster_df['cluster'].isin(kCluster)].index.values
     cell_outCluster = cluster_df.loc[cluster_df['cluster'].isin(vsCluster)].index.values
     print len(cell_inCluster), len(cell_outCluster)
